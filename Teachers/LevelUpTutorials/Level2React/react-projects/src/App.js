cbf3046da20d974d11419af0202afe966aa251fd
tranceyos2419 <tranceyos2419@gmail.com>
2018-10-04 00:22:41 +0900

    #14 Basic Transitions with React Spring

--- Teachers/LevelUpTutorials/Level2React/react-projects/src/App.js
+++ Teachers/LevelUpTutorials/Level2React/react-projects/src/App.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from 'react';
+import { Transition } from 'react-spring';
 import './App.css';
 
 //* I think I should use Elements
 import { Toggle } from 'Utilities';
-import { Modal } from 'Elements';
+import { Modal, Card } from 'Elements';
 import { UserProvider } from 'Contexts';
 
 import User from './User';
@@ -19,6 +20,24 @@ const App = () => (
         <h1 className="App-title">Welcome to React</h1>
       </header>
 
+      <section>
+        <Toggle>
+          {({ on, toggle }) => (
+            <Fragment>
+              <button onClick={toggle}>Show/Hide</button>
+              <Transition
+                from={{ opacity: 0 }}
+                enter={{ opacity: 1 }}
+                leave={{ opacity: 0 }}
+              >
+                {on && Header}
+
+              </Transition>
+            </Fragment>
+          )}
+        </Toggle>
+      </section>
+
       <Toggle>
         {({ on, toggle }) => (
           <Fragment>
@@ -31,6 +50,12 @@ const App = () => (
       </Toggle>
     </UserProvider>
   </div>
+);
+
+const Header = styles => (
+  <Card style={{ ...styles }}>
+    <h1>Show me</h1>
+  </Card>
 );
 
 export default App;
